f093c6e754b5288f0da7e6cad4b68ce4b1d69507
Marcin <marcin.sak.of@gmail.com>
2024-05-11 16:06:24 +0200

    ADD Strona główna - Dynamiczne ładowanie instytucji

--- charity_app/views.py
+++ charity_app/views.py
@@ -9,7 +9,12 @@ class LandingPageView(View):
     def get(self, request):
         total_bags_quantity = Donation.total_quantity_of_bags()
         number_of_institutions = Institution.count_institutions()
-        ctx = {'total_bags_quantity': total_bags_quantity, 'number_of_institutions': number_of_institutions}
+        local_organisations = Institution.objects.filter(type=3)
+        non_government_organisations = Institution.objects.filter(type=2)
+        foundations = Institution.objects.filter(type=1)
+        ctx = {'total_bags_quantity': total_bags_quantity, 'number_of_institutions': number_of_institutions,
+               'local_organisations': local_organisations, 'non_government_organisations': non_government_organisations,
+               'foundations': foundations}
         return render(request, 'index.html', context=ctx)
 
 
